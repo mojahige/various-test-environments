577a6ee75d9c6cc32439fd92a89fadd7a54ab072
mojahige <mojahige@gmail.com>
2022-01-26 09:29:29 +0900

    Initial commit from Create Next App

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:storybook/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+};
