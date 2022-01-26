f10b0619f6aa2106177796da06da254013e8acfc
mojahige <mojahige@gmail.com>
2022-01-26 10:12:36 +0900

    Add Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "storybook-addon-next",
+    "@storybook/addon-a11y",
+  ],
   framework: "@storybook/react",
   core: {
     builder: "webpack5",
